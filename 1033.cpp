c5ae528ce6ec96accb7256f143386261485df98c
GeZiyue <GeZiyue@users.noreply.github.com>
2019-08-03 00:14:43 +0800

    2019.08.02

---
+++ 1033.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <string>
+#include <cstring>
+#include <cstdlib>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+
+#define isNum(a) (a >= '0' && a <= '9')
+#define SP putchar(' ')
+#define EL putchar('\n')
+#define File(a) freopen(a ".in", "r", stdin), freopen(a ".out", "w", stdout)
+
+template<class T>
+inline void read(T&);
+template<class T>
+inline void write(const T&);
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef const int& ci;
+typedef std::pair<int, int> pii;
+const int iinf = 2147483647;
+const ll llinf = 9223372036854775807ll;
+using std::min;
+using std::max;
+using std::abs;
+using std::sort;
+
+int main () {
+    double h, s, v, l, k;
+    int n;
+    std::cin >> h >> s>> v >> l >> k;
+    read(n);
+    double t0 = sqrt((h - k) / 5);
+    double t1 = sqrt(h / 5);
+    int b = min(int(s - t0 * v + l), n);
+    int e = max(int(s - t1 * v), 0);
+    write(b - e), EL;
+    return 0;
+}
+
+template<class T>
+void read(T &Re) {
+    T k = 0;
+    char ch = getchar();
+    int flag = 1;
+    while (!isNum(ch)) {
+        if (ch == '-') {
+            flag = -1;
+        }
+        ch = getchar();
+    }
+    while (isNum(ch)) {
+        k = (k << 1) + (k << 3) + ch - '0';
+        ch = getchar();
+    }
+    Re = flag * k;
+}
+template<class T>
+void write(const T &Wr) {
+    if (Wr < 0) {
+        putchar('-');
+        write(-Wr);
+    } else {
+        if (Wr < 10) {
+            putchar(Wr + '0');
+        } else {
+            write(Wr / 10);
+            putchar((Wr % 10) + '0');
+        }
+    }
+}
